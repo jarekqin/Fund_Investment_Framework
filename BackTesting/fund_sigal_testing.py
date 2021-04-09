aacbca82edde24f40b39e9a69de26583357aeb92
jarekqin <loveboa007@live.cn>
2021-04-09 14:55:24 +0800

    add singal_testing functions

--- BackTesting/fund_sigal_testing.py
+++ BackTesting/fund_sigal_testing.py
@@ -8,21 +8,30 @@ from omk.core.vendor.RQData import RQData
 from Factors.Performance_Factor import PerformanceFacotr
 from WindPy import w
 
+
 class SingalFactorBackTesting:
-    def __init__(self,factor_name,init=True):
+    def __init__(self, factor_name,rolling_window=20, init=True):
         if init:
             w.start()
             RQData.init()
-        self.__factor_name=factor_name
+        self.__factor_name = factor_name
+        self.__rolling_window=rolling_window
 
-    def _get_data(self,data_source='wind'):
-        if data_source=='wind':
-            data=None
-        elif data_source=='rq':
-            data=None
+    def _get_data(self, data_source='wind'):
+        if data_source == 'wind':
+            data = None
+        elif data_source == 'rq':
+            data = None
             raise ValueError('Wrong data_source type!')
         return data
 
 
-    def main(self,data_source):
-        original_data=self._get_data(data_source)
+    def _back_testing(self):
+        pass
+
+    def main(self, data_source):
+        original_data = self._get_data(data_source)
+
+
+if __name__ == '__main__':
+    pass
